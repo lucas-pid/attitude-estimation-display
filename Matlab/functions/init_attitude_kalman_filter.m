dadb808fc30ca5744d1fff295a54de1d190d1099
lduarte <l.duarte@amazilia.aero>
2025-05-04 22:38:16 +0200

    Added linear acceleration compensation in kalman filter

--- Matlab/functions/init_attitude_kalman_filter.m
+++ Matlab/functions/init_attitude_kalman_filter.m
@@ -13,13 +13,14 @@ configuration_filter = filter_configuration();
 
 % A matrix
 % Assumption: Small angle approximation
-% States: Euler, g, rot_bias, acc_bias
-A = [zeros(3,3), zeros(3,1), -eye(3,3), zeros(3,3)
-    zeros(7,10)];
+% States:   Euler,              g,          rot_bias,       acc_bias,       linear_acceleration
+A = [       zeros(3,3),     zeros(3,1),     -eye(3,3),      zeros(3,3),         zeros(3,3)
+            zeros(7,13)
+            zeros(3,10),                                                        -eye(3)*0.5    ];
 
 % B Matrix
-B = [eye(3)
-    zeros(7,3)];
+B = [   eye(3)
+        zeros(10,3)];
 
 % Get O to B transformation and derivatives
 % Assume small angle
@@ -34,25 +35,27 @@ psi     = 0;
 g0 = [0;0;-9.81];
 
 % Observation matrix
-C = [M_B_O_dphi*g0, M_B_O_dtheta*g0, M_B_O_dpsi*g0, g0, zeros(3,3), eye(3)];
+C = [M_B_O_dphi*g0, M_B_O_dtheta*g0, M_B_O_dpsi*g0, g0, zeros(3,3), eye(3), eye(3)];
 
 % Disturbances B matrix
-% Disturbances: gyro noise, gyro bias noise, acc bias noise
-B_w     = [ eye(3),     zeros(3,6)                          % Euler
-            zeros(1,9)                                     % g
-            zeros(3,3), eye(3), zeros(3,3)      % Rot bias
-            zeros(3,3), zeros(3,3), eye(3)];    % Acc bias
+% Disturbances: gyro noise,     Rot bias noise,     acc bias noise,     linear acceleration noise 
+B_w     = [     eye(3),         zeros(3,9)                                                      % Euler
+                zeros(1,12)                                                                     % g
+                zeros(3,3),     eye(3),             zeros(3,6)                                  % Rot bias
+                zeros(3,3),     zeros(3,3),         eye(3),             zeros(3,3)              % Acc bias
+                zeros(3,9),                                             eye(3)];                % Linear acceleration
 
 % Process covariance
-rot_var         = 9e-3 * ones(3,1);
-acc_var         = 9e-3 * ones(3,1);
+rot_var         = 9e-5 * ones(3,1);
+meas_acc_var    = 9e-3 * ones(3,1);
+lin_acc_var     = 9e-2 * ones(3,1);
 rot_bias_var    = 1e-8 * ones(3,1);
 acc_bias_var    = 1e-8 * ones(3,1);
 
-Q = diag([rot_var; rot_bias_var; acc_bias_var]);
+Q = diag([rot_var; rot_bias_var; acc_bias_var; lin_acc_var]);
 
 % Measurement covariance
-R = diag(acc_var);
+R = diag(meas_acc_var);
 
 % Assemble filter struct with all matrices
 filt.sample_time_s = 1/100;
@@ -64,19 +67,20 @@ filt.R          = R;
 bias_gyro       = [0 0 0]';
 bias_acc        = [0 0 0]';
 g               = -9.81;
-filt.x0         = [deg2rad([0; 0; 0]); g; bias_gyro; bias_acc];
+filt.x0         = [deg2rad([0; 0; 0]); g; bias_gyro; bias_acc; zeros(3,1)];
 filt.C          = C;
 filt.I_n        = eye(length(A(:,1)));
 filt.x_num      = length(filt.Phi(:,1));
 filt.P0         = zeros(filt.x_num,filt.x_num);
-filt.Gamma_w_times_Q = diag(filt.Gamma_w * filt.Q * filt.Gamma_w');
 
 % Filter states idx
-filt.eul_idx = uint8(1:3);
-filt.g_idx = uint8(4);
-filt.rot_bias_idx = uint8(5:7);
-filt.acc_bias_idx = uint8(8:10);
-filt.rot_noise_idx = uint8(1:3);
+filt.eul_idx        = uint8(1:3);
+filt.g_idx          = uint8(4);
+filt.rot_bias_idx   = uint8(5:7);
+filt.acc_bias_idx   = uint8(8:10);
+filt.lin_acc_idx    = uint8(11:13);
+
+filt.rot_noise_idx  = uint8(1:3);
 
 % Transform doubles to single
 filt            = double2single(filt);
